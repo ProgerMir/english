c3da5e87363a49ba9869fcb4d49d1aa6b85ee0f3
ProgerMir <Dmi3ym@gmail.com>
2023-04-19 18:59:17 +0300

    changed a burger-button for mobile menu

--- dist/app.js
+++ dist/app.js
@@ -23,8 +23,6 @@ filter.on("click", function(event){
         });
     }
 
-    
-
 });
 
 
@@ -41,3 +39,25 @@ navToggle.on("click", function(event){
 
 });
 });
+
+
+// Icons for mobile-menu
+
+const burgerIcon = document.querySelector('.burger__icon')
+
+burgerIcon.addEventListener('click', function(){
+    const burgerIconActive = burgerIcon.classList.toggle('active')
+})
+
+// const burgerIconCross = burgerIcon.addEventListener('click', function() {
+//     const burgerIconActive = burgerIcon.classList.add('burger__icon--active')
+//     burgerIcon.setAttribute('src', './images/cross.svg');
+//     if(burgerIconActive){
+//         burgerIcon.setAttribute('src', './images/cross.svg')
+//     }else{
+//         burgerIcon.setAttribute('src', './images/burger.svg')
+//     }
+// } 
+
+// )
+
